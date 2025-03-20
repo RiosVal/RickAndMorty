13c4bfa857d7ab0e72130c09495c19d3ab253e86
Valeria <riosh.valeria@gmail.com>
2025-03-20 11:01:43 -0500

    "First commit"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [
+    IonApp,
+    IonRouterOutlet
+  ],
 })
 export class AppComponent {
   constructor() {}
