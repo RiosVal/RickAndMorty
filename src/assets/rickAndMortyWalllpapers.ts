cb3d0ab1d473110e68353f95020f0357dbf79efd
Valeria <riosh.valeria@gmail.com>
2025-03-25 11:47:06 -0500

    added wallpapers urls

---
+++ src/assets/rickAndMortyWalllpapers.ts
@@ -0,0 +1,7 @@
+export const RICK_AND_MORTY_WALLPAPERS: any = [
+    "https://wallpapers.com/images/high/ufo-rick-and-morty-4k-m0u0svggs60tfle6.webp",
+    "https://wallpapers.com/images/high/demogorgon-stranger-things-rick-and-morty-4k-iq97cmbr5c11d0i1.webp",
+    "https://wallpapers.com/images/high/rick-and-morty-cool-portrait-background-8m89hskh03j1h2qh.webp",
+    "https://wallpapers.com/images/high/rick-and-morty-phone-characters-j7r684kpbk7wqjlb.webp",
+    "https://wallpapers.com/images/high/rick-and-morty-in-a-colorful-planet-zh6hjd4zrielptni.webp"
+]
