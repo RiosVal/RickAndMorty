010f60dd922be4a2bba0b2de0b3fa105dca20e52
Valeria <riosh.valeria@gmail.com>
2025-03-25 10:00:04 -0500

    added methods to combine episodes images in response

--- src/app/services/rickandmorty.service.ts
+++ src/app/services/rickandmorty.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, switchMap, forkJoin, of } from 'rxjs';
+import { IMAGES_EPISODES } from 'src/assets/imageEpisodes';
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +21,47 @@ export class RickandmortyService {
     return this.http.get<any>(`https://rickandmortyapi.com/api/character/${id}`);
   }
   
+  getEpisodesWithImages(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/episode`).pipe(
+      switchMap((response: any) => {
+        const allEpisodes = this.getAllEpisodes(response);
+        return allEpisodes.pipe(
+          map(episodes => this.addImagesToEpisodes(episodes))
+        );
+      })
+    );
+  }
+
+  private getAllEpisodes(firstPage: any): Observable<any[]> {
+    const pages = firstPage.info.pages;
+    const requests = [];
+    
+    for(let i = 1; i <= pages; i++) {
+      requests.push(this.http.get(`${this.apiUrl}/episode?page=${i}`));
+    }
+
+    return forkJoin(requests).pipe(
+      map((responses: any[]) => {
+        return responses.reduce((acc, curr) => 
+          acc.concat(curr.results), []);
+      })
+    );
+  }
+
+  private addImagesToEpisodes(episodes: any[]): any[] {
+    return episodes.map(episode => {
+      const episodeCode = episode.episode.substring(1);
+      const [season, episodeNumber] = episodeCode.split('E');
+      
+      const imageData = IMAGES_EPISODES.find((img: { season: number; episode: number; }) => 
+        img.season === parseInt(season) && 
+        img.episode === parseInt(episodeNumber)
+      );
+
+      return {
+        ...episode,
+        image_url: imageData?.image_url || null
+      };
+    });
+  }
 }
--- src/app/tab-episodes/tab-episodes.page.ts
+++ src/app/tab-episodes/tab-episodes.page.ts
@@ -1,20 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { 
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonThumbnail,
+  IonSpinner,
+  IonAccordionGroup,
+  IonAccordion } from '@ionic/angular/standalone';
+import { RickandmortyService } from '../services/rickandmorty.service';
 
 @Component({
   selector: 'app-tab-episodes',
   templateUrl: './tab-episodes.page.html',
   styleUrls: ['./tab-episodes.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent, IonHeader, IonTitle, IonToolbar,
+    IonAccordionGroup, IonAccordion, IonItem,
+    IonLabel, IonThumbnail, IonSpinner, IonList,
+    CommonModule, FormsModule
+  ]
 })
 export class TabEpisodesPage implements OnInit {
+  episodes: any[] = [];
+  loading: boolean = true;
+  error: any = null;
 
-  constructor() { }
+  constructor(private rickandmortyService: RickandmortyService) { }
 
   ngOnInit() {
+    this.loadEpisodes();
   }
 
+  loadEpisodes() {
+    this.loading = true;
+    this.error = null;
+    
+    this.rickandmortyService.getEpisodesWithImages().subscribe({
+      next: (data: any[]) => {
+        this.episodes = data;
+        this.loading = false;
+        console.log(this.episodes);
+      },
+      error: (err: any) => {
+        this.error = err;
+        this.loading = false;
+        console.error('Error loading episodes:', err);
+      }
+    });
+  }
 }
