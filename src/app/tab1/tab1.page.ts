8578ef440eb6d34d519cb19e675bbf917dbdaeb8
Valeria <riosh.valeria@gmail.com>
2025-03-26 11:43:32 -0500

    added swiper on tab1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { RICK_AND_MORTY_WALLPAPERS } from 'src/assets/rickAndMortyWalllpapers';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab1Page {
+  wallpapers = RICK_AND_MORTY_WALLPAPERS;
   constructor() {}
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { provideHttpClient } from '@angular/common/http';
+import { register as registerSwiperElements } from 'swiper/element/bundle';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -20,6 +21,8 @@ addIcons({
   'arrow-back-outline': arrowBackOutline,
   'trash-outline': trashOutline
 });
+
+registerSwiperElements();
 
 bootstrapApplication(AppComponent, {
   providers: [
